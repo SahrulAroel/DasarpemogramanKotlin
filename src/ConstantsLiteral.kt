15d3ede8fbc9d7556da34763b5b3d823eb7116e2
LENOVO <sahrulcc07@gmail.com>
2024-09-03 13:55:05 +0700

    Add ConstantsLiteral.kt

---
+++ src/ConstantsLiteral.kt
@@ -0,0 +1,13 @@
+fun main(args: Array<String>) {
+    val floatLiteral : Float = 178.95F
+    println("Contoh Literal Float : "+ floatLiteral)
+    val oneBillion: Long = 100000000L
+    println("Contoh Literal Long : "+ oneBillion)
+    val valHeksa = 0x0F
+    println("Contoh Literal Heksadesimal : " + valHeksa)
+    val valBinary = 0b0001011
+    println("Contoh Literal Binary : "+ valBinary)
+    val notasiKonvensional = 123.5e10
+    println("Contoh Literal Konvensional Floating Point Number : "+ notasiKonvensional)
+
+}
